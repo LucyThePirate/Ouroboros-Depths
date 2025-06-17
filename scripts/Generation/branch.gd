f560ea3f94264143e3caeacda0de4f9eb7cb5bef
LucyThePirate <artistic109@gmail.com>
2025-06-17 16:58:12 -0500

    Added ability to check for ending rooms

--- scripts/Generation/branch.gd
+++ scripts/Generation/branch.gd
@@ -8,6 +8,9 @@ var left_child: Branch  # yes, these are self referential :D
 var right_child: Branch
 var position: Vector2i
 var size: Vector2i
+var is_ending_branch := false
+var split_iteration := -1
+var path_intersection_count := 0
 
 
 func _init(position, size):
@@ -17,16 +20,19 @@ func _init(position, size):
 
 func get_leaves():
 	if not (left_child && right_child):
+		self.is_ending_branch = true
 		return [self]
 	else:
+		self.is_ending_branch = false
 		return left_child.get_leaves() + right_child.get_leaves()
 
 
 func split(remaining, paths):
+	self.split_iteration = remaining
 	var rng = RandomNumberGenerator.new()
 	var split_percent = rng.randf_range(0.3, 0.7)  # splits will be between 30% and 70%
 	var split_horizontal = size.y >= size.x  # if it is taller than it is wide
-	
+
 	if split_horizontal:
 		# horizontal
 		var left_height = int(size.y * split_percent)
@@ -43,10 +49,9 @@ func split(remaining, paths):
 		)
 
 	if remaining > 0:
-		paths.push_back({'left': left_child.get_center(), 'right': right_child.get_center()})
+		paths.push_back({"left": left_child.get_center(), "right": right_child.get_center()})
 		left_child.split(remaining - 1, paths)
 		right_child.split(remaining - 1, paths)
-	pass
 
 
 func get_center():
--- scripts/Generation/cell.gd
+++ scripts/Generation/cell.gd
@@ -9,6 +9,8 @@ var tile_size: int = 100
 @onready var room_floor_tile := [3, Vector2i(0, 3)]
 @onready var stone_wall_tile := [2, Vector2i(1, 0)]
 @onready var stone_floor_tile := [2, Vector2i(0, 1)]
+@onready var door_horizontal_tile := [2, Vector2i(1, 2)]
+@onready var door_vertical_tile := [2, Vector2i(2, 2)]
 
 var paths: Array = []
 
@@ -45,11 +47,12 @@ func _draw():
 				var tile_coordinate = Vector2i(x + leaf.position.x, y + leaf.position.y)
 				if not is_inside_padding(x, y, leaf, padding):
 					floors.set_cell(tile_coordinate, room_floor_tile[0], room_floor_tile[1])
-				else:
+				else:  # Wall
 					floors.set_cell(tile_coordinate, stone_floor_tile[0], stone_floor_tile[1])
 					walls.set_cell(tile_coordinate, stone_wall_tile[0], stone_wall_tile[1])
 					# here Vector2i(2, 2) is where our floor is in the tileset we are using
 		for path in paths:
+			draw_line(path["left"] * tile_size, path["right"] * tile_size, Color.RED, 10)
 			if path["left"].y == path["right"].y:
 				# horizontal
 				for i in range(path["right"].x - path["left"].x):
@@ -58,6 +61,15 @@ func _draw():
 					if walls.get_cell_tile_data(tile_coordinate):
 						floors.set_cell(tile_coordinate, room_floor_tile[0], room_floor_tile[1])
 						walls.set_cell(tile_coordinate, -1)
+
+						if (
+							walls.get_cell_tile_data(tile_coordinate - Vector2i(0, 1))
+							and walls.get_cell_tile_data(tile_coordinate + Vector2i(0, 1))
+						):
+							leaf.path_intersection_count += 1
+							objects.set_cell(
+								tile_coordinate, door_vertical_tile[0], door_vertical_tile[1]
+							)
 			else:
 				# vertical
 				for i in range(path["right"].y - path["left"].y):
@@ -66,7 +78,22 @@ func _draw():
 					if walls.get_cell_tile_data(tile_coordinate):
 						floors.set_cell(tile_coordinate, room_floor_tile[0], room_floor_tile[1])
 						walls.set_cell(tile_coordinate, -1)
-	pass
+
+						if (
+							walls.get_cell_tile_data(tile_coordinate - Vector2i(1, 0))
+							and walls.get_cell_tile_data(tile_coordinate + Vector2i(1, 0))
+						):
+							leaf.path_intersection_count += 1
+							objects.set_cell(
+								tile_coordinate, door_horizontal_tile[0], door_horizontal_tile[1]
+							)
+	for leaf in root_node.get_leaves():
+		print("leaf path count:", leaf.path_intersection_count)
+		for x in range(leaf.size.x):
+			for y in range(leaf.size.y):
+				var tile_coordinate = Vector2i(x + leaf.position.x, y + leaf.position.y)
+				if leaf.path_intersection_count != 1:
+					floors.set_cell(tile_coordinate, stone_floor_tile[0], stone_floor_tile[1])
 
 
 func is_inside_padding(x, y, leaf, padding):
